63b0803549a53dfeed8974757937b8ca358ed0cf
damianarakiel <damian.stefanski@hotmail.com>
2018-11-09 00:09:18 +0100

    Bibliotek v0.9

--- src/main/java/app/LibraryApp.java
+++ src/main/java/app/LibraryApp.java
@@ -2,7 +2,7 @@ package app;
 
 public class LibraryApp {
 
-    public static final String APP_NAME = "Bibliotek v0.9";
+    public static final String APP_NAME = "Bibliotek v0.9"; // wersja aplikacji
 
     public static void main(String[] args) {
 
